99a854c4a7a93767699912d567e832559b3515a9
Jay Rome Rana <100551254+moochh@users.noreply.github.com>
2024-04-03 17:02:07 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 function calc() {
   var entry = document.getElementById("entry").value
   var stopLoss = document.getElementById("stopLoss").value
-  var initialCost = document.getElementById("initialCost").value
-  var risk = document.getElementById("riskAmount").value
+  var accountCapital = document.getElementById("accountCapital").value
+  var riskPercentage = document.getElementById("riskPercentage").value
+
+  if (riskPercentage == "") {
+    var risk = 5
+  } else {
+    var risk = accountCapital * riskPercentage / 100;
+  }
 
   var quantity = Math.abs(risk/(entry-stopLoss))
-  var leverage = quantity*entry/initialCost
+  var leverage = quantity*entry/risk/3
 
   document.getElementById("quantity").innerHTML = "Quantity: " + quantity.toFixed(3)
-  document.getElementById("leverage").innerHTML = "Leverage: " + Math.ceil(leverage) + "x"
+  document.getElementById("leverage").innerHTML = "Leverage: " + leverage.toFixed(1) + "x"
 }
 
 
